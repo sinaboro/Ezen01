e13ac7c76087e3f755b933a3bce9a335e3e9b254
gitTest <rlaeocjf@gmail.com>
2022-04-22 09:48:02 +0900

    Day20220422

--- Day20220421/src/Ex07.java
+++ Day20220421/src/Ex07.java
@@ -6,31 +6,27 @@ public class Ex07 {
 
 	public static void main(String[] args) {
 	
-		// int[] nArr = new int[10] ; ==> 난수 (1~10)를 배열 입력
-		//단, 중복된 숫자가 없어야 한다.
+		Scanner sc  =  new Scanner(System.in);
 		
-		Random rand = new Random(7);
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("배열 갯수 입력 : ");
-		int n = sc.nextInt();
-		int[] nArr = new int[n];
-		
-		nArr[0] = rand.nextInt(10);
-		
-		for(int i=0; i<nArr.length; i++) {
-			int j;
-			do {
-				j=0;
-				nArr[i] = rand.nextInt(10) + 1;
-				
-				for(; j<i; j++)
-					if(nArr[j] == nArr[i])
-						break;
-			}while(j<i);
-		}
-		
+		int n = sc.nextInt()   ;
+		int[] nArr = new int[n]   ;
+		int j ;
+
+		nArr[0] = (int)(Math.random()*10);
+
+		for(int i = 1; i < nArr.length; i++) {  
+
+		   nArr[i] = (int)(Math.random()*10);
+		   
+		  for(j = 0 ; j <i ; j++) {  
+		      if(nArr[j]==nArr[i])
+			  i--;
+		       	//break;
+		      
+		     
+		  } //for j
+
+		} // for i
 		System.out.println(Arrays.toString(nArr));
 	}
-
 }

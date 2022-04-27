41c3de0988d63924ad5143e3f3c4ec9e98473d03
gitTest <rlaeocjf@gmail.com>
2022-04-27 11:52:05 +0900

    day0427

--- Day20220427/src/CarTest.java
+++ Day20220427/src/CarTest.java
@@ -3,12 +3,13 @@ import lombok.ToString;
 public class CarTest {
 	public static void main(String[] args) {
 		
-		Sedan se = new Sedan();
-		se.color = "white";
-		se.passenger = 5;
-		se.tire = 4;
-		System.out.println(se.color + ", " + se.passenger + "," + se.tire);
+		Sedan se = new Sedan("white",5,4);
+//		se.color = "white";
+//		se.passenger = 5;
+//		se.tire = 4;
 		
+		System.out.println(se.color + ", " + se.passenger + "," + se.tire);
+		se.run();
 		Bus bu = new Bus();
 		bu.passenger = 15;
 		bu.tire = 6;
@@ -16,19 +17,36 @@ public class CarTest {
 	}
 }
 
-@ToString
+class Sedan extends Car{
+	String color;
+	
+	public Sedan(String color, int passenger, int tire) {
+		super(passenger,tire,0);
+		this.color = color;
+//		this.passenger = passenger;
+//		this.tire = tire;
+		System.out.println("Sedan() 생성자");
+	}
+}
 class Car {
 	int passenger;
 	int tire;
 	int seat;
-	void run() {}
+	public Car(int passenger, int tire, int seat) {
+		this.passenger = passenger;
+		this.tire = tire;
+		this.seat = seat;
+		System.out.println("Car() 생성자");
+	}
+	public Car() {};
+	
+	void run() {
+		System.out.println("run()");
+	}
 	void stop() {}
+	
 }
 
-@ToString
-class Sedan extends Car{
-	String color;
-}
 
 @ToString
 class Bus extends Car{

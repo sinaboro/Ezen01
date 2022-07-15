5b8227d2880b042ac267df32fb7a76ac2f2f7427
free <rlaeocjf@gmail.com>
2022-07-15 13:12:12 +0900

    0715-3

--- ex02/src/main/java/org/zerock/controller/BoardController.java
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -3,7 +3,10 @@ package org.zerock.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
 import org.zerock.service.BoardService;
 
 import lombok.AllArgsConstructor;
@@ -23,5 +26,35 @@ public class BoardController {
 		log.info("list---------------------");
 		model.addAttribute("list", service.getList());
 	}
+	
+	@PostMapping("/register")
+	public String register(BoardVO vo, RedirectAttributes rttr) {
+		log.info("register : " + vo);
+		service.register(vo);
+		rttr.addFlashAttribute("result", vo.getBno());
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping("/get")
+	public void get(Long bno, Model model) {
+		log.info("get.....................");
+		model.addAttribute("board", service.get(bno));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 		
 }
---
+++ ex02/src/main/webapp/WEB-INF/views/board/register.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<title>Insert title here</title>
+</head>
+<body>
+
+	<h1>Register Page</h1>
+</body>
+</html>

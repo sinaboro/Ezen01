f5170ee6081363893d47dee6d1044de7b980a629
free <rlaeocjf@gmail.com>
2022-07-20 11:01:49 +0900

    0720-2

--- ex02/src/main/java/org/zerock/controller/BoardController.java
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -3,6 +3,7 @@ package org.zerock.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -47,26 +48,31 @@ public class BoardController {
 	}
 	
 	@GetMapping({"/get", "/modify"})
-	public void get(Long bno, Model model) {
+	public void get(Long bno,  @ModelAttribute("cri") Criteria cri , Model model) {
 		log.info("--------------get or modify--------------");
 		model.addAttribute("board", service.get(bno));
 	}
 	
 	@PostMapping("/remove")
-	public String remove(Long bno, RedirectAttributes rttr) {
+	public String remove(Long bno, @ModelAttribute("cri") Criteria cri, RedirectAttributes rttr) {
 		log.info("remove---------" + bno);
 		if(service.remove(bno)==1) {
 			rttr.addFlashAttribute("result", "success");
 		}
+		
+		rttr.addAttribute("pageNum", cri.getPageNum());
+		rttr.addAttribute("amount", cri.getAmount());
 		return "redirect:/board/list";
 	}
 	
 	@PostMapping("/modify")
-	public String modify(BoardVO vo, RedirectAttributes rttr) {
+	public String modify(BoardVO vo, @ModelAttribute("cri") Criteria cri, RedirectAttributes rttr) {
 		log.info("modify : " + vo);
 		if( service.modify(vo) == 1) {
 			rttr.addFlashAttribute("result", "success");
 		}
+		rttr.addAttribute("pageNum", cri.getPageNum());
+		rttr.addAttribute("amount", cri.getAmount());
 		return "redirect:/board/list";
 	}
 	
--- ex02/src/main/webapp/WEB-INF/views/board/modify.jsp
+++ ex02/src/main/webapp/WEB-INF/views/board/modify.jsp
@@ -22,6 +22,10 @@
                         <div class="panel-body">
                         
                         	<form role="form"  action="/board/modify"  method="post">
+                        	
+                        		<input type="hidden" name="pageNum" value='<c:out value="${cri.pageNum}"/>'   >
+						    	<input type="hidden" name="amount" value='<c:out value="${cri.amount}"/>'   >
+								
 								<div class="from-group">
 									<label>Bno</label>
 									<input class="form-control"  name="bno"  readonly="readonly"  value='<c:out value="${board.bno}"/>' ><br>
@@ -83,9 +87,12 @@
  				if(operation === 'remove'){
  					formObj.attr("action", "/board/remove");
  				}else if(operation === 'list'){
- 				//	self.location = "/board/list";
- 				formObj.attr("action", "/board/list").attr("method","get");
- 				formObj.empty();
+	 				formObj.attr("action", "/board/list").attr("method","get");
+	 				var pageNumTag = $("input[name='pageNum']").clone();
+	 				var amountTag = $("input[name='amount']").clone();
+	 				formObj.empty();
+	 				formObj.append(pageNumTag);
+	 				formObj.append(amountTag);
  				}
  				formObj.submit();
  			});
